3895cacc3c05fde9efc94d16f80ae06803c882da
CMCarlaMecatti <canmecatti@gmail.com>
2023-09-25 10:23:54 -0300

    commit todos iguales

--- todosIguales.js
+++ todosIguales.js
@@ -2,4 +2,5 @@ const todosIguales = (array, array2) => {
     // Si todos los elementos del arreglo son iguales, retornar true.
    // Caso contrario retornar false.
    // Tu código:
+   return array.every(element => array2.includes(element))
 }
