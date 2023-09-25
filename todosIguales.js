4fb4be0e7120db092a5842353da7b133e5d49fb9
Juan Diaz <juanjoc2000@hotmail.com>
2023-09-25 10:27:00 -0300

    prueba

--- todosIguales.js
+++ todosIguales.js
@@ -1,5 +1,14 @@
-const todosIguales = (array, array2) => {
-    // Si todos los elementos del arreglo son iguales, retornar true.
-   // Caso contrario retornar false.
-   // Tu código:
-}
+const todosIguales = (array) => {
+  // Si todos los elementos del arreglo son iguales, retornar true.
+  // Caso contrario retornar false.
+  // Tu código:
+  let igual = true;
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] === array[0]) {
+      igual = true;
+    } else {
+      igual = false;
+    }
+  }
+  return igual;
+};
