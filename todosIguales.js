b3a1bae136d3848240ca86be2f42e831cf10a6c8
Uriel Campos <urielcampos448@gmail.com>
2023-09-25 10:20:06 -0300

    resuelto

--- todosIguales.js
+++ todosIguales.js
@@ -1,5 +1,6 @@
-const todosIguales = (array, array2) => {
+const todosIguales = (array, ) => {
     // Si todos los elementos del arreglo son iguales, retornar true.
    // Caso contrario retornar false.
    // Tu código:
+   array.every(elemento => elemento === array[0]);
 }
