fc405525ca39d7d5ac6312700188657ed97d4229
fof3096 <c0rr1f1ll@gmail.com>
2023-09-25 10:32:04 -0300

    si

--- todosIguales.js
+++ todosIguales.js
@@ -1,5 +1,21 @@
 const todosIguales = (array, array2) => {
-    // Si todos los elementos del arreglo son iguales, retornar true.
-   // Caso contrario retornar false.
-   // Tu código:
-}
+  // Si todos los elementos del arreglo son iguales, retornar true.
+  // Caso contrario retornar false.
+  // Tu código:
+  if (array.length !== array2.length) {
+    return false;
+  }
+
+  let absoluteArray = new Set();
+  let i = 0;
+  while (i <= array.length) {
+    absoluteArray.add(array[0]);
+    absoluteArray.add(array2[0]);
+    i++;
+  }
+  if ([...absoluteArray].length == array.length) {
+    return true;
+  } else {
+    return false;
+  }
+};
